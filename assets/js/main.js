e3ecbfe8c3dbc920641b11b713b130e9d9ee1715
kevinMateus <kevinsmateus.outlook.com>
2024-06-05 00:16:44 +0100

    feat(LocalStorage/CompletedTaskFlow):add localstorage, complete task handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,108 @@
-const handleTasks = () => {
-  const form = document.querySelector("#formElement");
-  const tasksContainer = document.querySelector("#tasksContainer");
+let tasks = [];
+
+const handleUpdateTaskBadges = () => {
   const createdTasksBadge = document.querySelector("#createdTasksBadge");
   const completedTasksBadge = document.querySelector("#completedTasksBadge");
 
-  if (!form || !tasksContainer || !createdTasksBadge || !completedTasksBadge)
+  if (!completedTasksBadge || !createdTasksBadge) {
     return;
+  }
+  createdTasksBadge.textContent = tasks.length;
 
-  // Initial state
-  // local storage
-  const tasks = [
-    {
-      message: "task 1",
-      completed: false,
-    },
-    {
-      message: "task 2",
-      completed: false,
-    },
-  ];
+  const completedTasks = tasks.filter((task) => task.completed);
 
-  const handleUpdateTaskBadges = () => {
-    createdTasksBadge.textContent = tasks.length;
+  completedTasksBadge.textContent = `${completedTasks.length} de ${tasks.length}`;
+};
 
-    const completedTasks = tasks.filter((task) => task.completed);
+const handleCompleteTask = (taskIndex) => {
+  console.log(`completed task ${taskIndex}`);
+  const getMemoryTask = tasks[taskIndex];
+  const taskElement = document.querySelector(`#task-item-${taskIndex}`);
 
-    completedTasksBadge.textContent = `${completedTasks.length} de ${tasks.length}`;
+  if (!taskElement || !getMemoryTask) {
+    return;
+  }
+
+  // Change completed property in memory
+  getMemoryTask.completed = true;
+
+  // Update LocalStorage task
+
+  const getLocalStorageTasks = window.localStorage.getItem("tasks");
+  if (!getLocalStorageTasks) {
+    return;
+  }
+
+  const parsedTasks = JSON.parse(getLocalStorageTasks);
+
+  const getLsTask = parsedTasks[taskIndex];
+
+  if (!getLsTask) {
+    return;
+  }
+
+  getLsTask.completed = true;
+
+  window.localStorage.setItem("tasks", JSON.stringify(parsedTasks));
+
+  // add completed task to html
+  taskElement.classList.add("completed");
+
+  handleUpdateTaskBadges();
+};
+
+const handleTasks = () => {
+  const form = document.querySelector("#formElement");
+  const tasksContainer = document.querySelector("#tasksContainer");
+
+  if (!form || !tasksContainer) {
+    return;
+  }
+
+  const handleLocalStorageInit = () => {
+    const getLocalStorageTasks = window.localStorage.getItem("tasks");
+
+    if (!getLocalStorageTasks) {
+      window.localStorage.setItem("tasks", JSON.stringify([]));
+      return;
+    }
+    const parsedTasks = JSON.parse(getLocalStorageTasks);
+
+    console.log("parsedTasks", parsedTasks);
+
+    tasks = parsedTasks;
   };
+
+  const addNewLsTask = (newTaskObj) => {
+    const getLocalStorageTasks = window.localStorage.getItem("tasks");
+    if (!getLocalStorageTasks) {
+      return;
+    }
+
+    const parsedTasks = JSON.parse(getLocalStorageTasks);
+
+    parsedTasks.push(newTaskObj);
+
+    window.localStorage.setItem("tasks", JSON.stringify(parsedTasks));
+  };
+
+  handleLocalStorageInit();
 
   handleUpdateTaskBadges();
 
   console.log("tasks", tasks);
 
-  for (let index = 0; index < tasks.length; index++) {
-    const task = tasks[index];
+  for (let taskIndex = 0; taskIndex < tasks.length; taskIndex++) {
+    const task = tasks[taskIndex];
 
     tasksContainer.insertAdjacentHTML(
       "beforeend",
-      `<li class="task-item">
-    <button>
-      <img src="assets/media/check.svg" />
+      `<li class="task-item ${
+        task.completed ? `completed` : ""
+      }" id="task-item-${taskIndex}">
+    <button class="completeTaskBtn" onclick="handleCompleteTask(${taskIndex})">
+      <img class="uncompleted-icon" src="assets/media/check.svg" />
+      <img class="completed-icon" src="assets/media/checked.svg" />
     </button>
     <p>${task.message}</p>
     <button>
@@ -56,25 +119,35 @@ const handleTasks = () => {
 
     const inputValue = formData.get("add-task");
 
-    tasks.push({
+    const newTaskObj = {
       message: inputValue,
       completed: false,
-    });
+    };
+
+    //add in memory
+    tasks.push(newTaskObj);
 
     document.querySelector("#add-task").value = "";
 
+    //add to HTML
+
     tasksContainer.insertAdjacentHTML(
       "beforeend",
-      `<li class="task-item">
-    <button>
-      <img src="assets/media/check.svg" />
-    </button>
-    <p>${inputValue}</p>
-    <button>
-      <img src="assets/media/trash.svg" />
-    </button>
-  </li>`
+      `<li class="task-item" id="task-item-${tasks.length - 1}">
+      <button class="completeTaskBtn" onclick="handleCompleteTask("${
+        tasks.length - 1
+      }")>
+      <img class="uncompleted-icon" src="assets/media/check.svg" />
+      <img class="completed-icon" src="assets/media/checked.svg" />
+      </button>
+      <p>${inputValue}</p>
+      <button>
+        <img src="assets/media/trash.svg" />
+      </button>
+    </li>`
     );
+
+    addNewLsTask(newTaskObj);
 
     handleUpdateTaskBadges();
   };
